9dc234de7edcbad6ca7c1c9bdf575663b7b9714c
HaydenMcT <hayden.mctavish@alumni.ubc.ca>
2025-08-12 13:10:26 -0700

    Add pystreed trees; update convention for handling node_ids in dict conversions

--- tests/test_dict_to_sklearn.py
+++ tests/test_dict_to_sklearn.py
@@ -1,8 +1,9 @@
 from utils.tree_dict_to_sklearn import construct_sklearn_tree_from_dict
 from sklearn.tree import DecisionTreeClassifier
-from utils.tree_to_dict import sklearn_tree_to_dict, dl85_to_dict, gosdt_to_dict
+from utils.tree_to_dict import sklearn_tree_to_dict, dl85_to_dict, gosdt_to_dict, pystreed_tree_to_dict
 from gosdt import GOSDTClassifier as GOSDT
 import numpy as np
+from pystreed import STreeDClassifier
 
 def test_sklearn_to_sklearn_predictive_validity():
     # Create and fit a dummy tree
@@ -33,9 +34,23 @@ def test_gosdt_to_sklearn_predictive_validity():
 
     # Check that we can map from sklearn to a dict and back
     new_clf = construct_sklearn_tree_from_dict(
-        gosdt_to_dict(clf.trees_[0].tree)[0], 
+        gosdt_to_dict(clf.trees_[0].tree), 
         X_dummy.shape[1]
     )
 
     X_dummy = np.random.randint(0, 2, size=(10_000, 6))
+    assert (new_clf.predict(X_dummy) == clf.predict(X_dummy)).all()
+
+def test_pystreed_to_sklearn_predictive_validity():
+    # Create and fit a dummy tree
+    X_dummy = np.random.rand(100, 17) < 0.5
+    y_dummy = np.random.randint(0, 2, size=100)
+    clf = STreeDClassifier(max_depth=3, max_num_nodes=3)
+    clf.fit(X_dummy, y_dummy)
+
+    dict_tree = pystreed_tree_to_dict(clf.tree_)
+
+    new_clf = construct_sklearn_tree_from_dict(dict_tree, X_dummy.shape[1])
+
+    X_dummy = np.random.rand(10_000, 17)
     assert (new_clf.predict(X_dummy) == clf.predict(X_dummy)).all()
--- utils/tree_to_dict.py
+++ utils/tree_to_dict.py
@@ -53,7 +53,7 @@ def dl85_to_dict(node):
         dict_tree['false'] = dl85_to_dict(node['right'])
         return dict_tree
         
-def gosdt_to_dict(node, next_node_id=0):
+def _gosdt_to_dict(node, next_node_id=0):
     '''
     Convert a GOSDT tree to a dictionary representation.
     This dictionary representation allows constructing
@@ -75,6 +75,33 @@ def gosdt_to_dict(node, next_node_id=0):
     else:
         dict_tree['threshold'] = 0.5
         dict_tree['feature'] = node.feature
-        dict_tree['false'], right_node_id = gosdt_to_dict(node.right_child, next_node_id + 1)
-        dict_tree['true'], left_node_id = gosdt_to_dict(node.left_child, right_node_id + 1)
+        dict_tree['false'], right_node_id = _gosdt_to_dict(node.right_child, next_node_id + 1)
+        dict_tree['true'], left_node_id = _gosdt_to_dict(node.left_child, right_node_id + 1)
         return dict_tree, left_node_id
+
+def gosdt_to_dict(tree):
+    dict_tree, _ = _gosdt_to_dict(tree)
+    return dict_tree
+
+def pystreed_tree_to_dict(tree) -> dict:
+    def _recurse_to_dict(node_obj, node_id):
+        node = {}
+        node['id'] = node_id
+        ####################
+        #features needed for dnf_trees:
+        node['reference'] = 'true' #queries assumed to always correspond to whether a feature is True;
+                                   # since we're expecting to be dealing with binarized data always with
+                                   # thresholds of 0, where right/true corresponds to > 0, this should always hold
+        node['relation'] = '=='
+        ####################
+        if node_obj.is_leaf_node():
+            node['prediction'] = node_obj.label
+            largest_id_in_subtree = node_id
+        else:
+            node['feature'] = node_obj.feature
+            node['threshold'] = 0 # threshold should always be 0 for binarized data; TODO verify
+            node['false'], largest_id_in_right_child = _recurse_to_dict(node_obj.right_child, node_id+1)
+            node['true'], largest_id_in_subtree = _recurse_to_dict(node_obj.left_child, largest_id_in_right_child + 1) #TODO verify left/right
+        return node, largest_id_in_subtree
+
+    return _recurse_to_dict(tree, 0)[0]
